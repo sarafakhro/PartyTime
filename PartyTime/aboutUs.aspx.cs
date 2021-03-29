b4801db4484764b81e0934d43ae881f1fc0de668
Sara <sara.fakhro@outlook.com>
2021-03-29 14:11:05 +0200

    Jag har designat och gjort funktionerna för dynamiska knappar för "logIn" och "signUp".

--- PartyTime/aboutUs.aspx.cs
+++ PartyTime/aboutUs.aspx.cs
@@ -11,7 +11,32 @@ namespace PartyTime
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            logInBtn.ServerClick += logInBtn_ServerClick;
+            signUpBtn.ServerClick += signUpBtn_ServerClick;
+            logOutBtn.ServerClick += logOutBtn_ServerClick;
+            dashBtn.ServerClick += dashBtn_ServerClick;
         }
+
+        private void dashBtn_ServerClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void logOutBtn_ServerClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void signUpBtn_ServerClick(object sender, EventArgs e)
+        {
+            Response.Redirect("signUp.aspx");
+        }
+
+        private void logInBtn_ServerClick(object sender, EventArgs e)
+        {
+            Response.Redirect("logIn.aspx");
+        }
+
+
     }
 }
