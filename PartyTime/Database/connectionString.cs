ace20988f57763b9943a1eb5dbf1545b4a19fbbb
Bojana Filipovic <al0990@mau.se>
2021-03-30 11:55:48 +0200

    Skapat databas connection string

---
+++ PartyTime/Database/connectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace PartyTime.Database
+{
+    public class connectionString
+    {
+        public static string con = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+    }
+}
