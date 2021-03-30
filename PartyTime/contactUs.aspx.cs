5f7aeb822222066fa3f33455d03bb2a98eae1adc
Bojana Filipovic <al0990@mau.se>
2021-03-30 11:36:03 +0200

    Fixade logik för knapparna så dom finns eller göms baserad på logged in user

--- PartyTime/contactUs.aspx.cs
+++ PartyTime/contactUs.aspx.cs
@@ -1,14 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-  namespace PartyTime
+using System.Web.UI;
+
+namespace PartyTime
 {
     public partial class contactUs : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(Session["logged User"] != null)
+            {
+                dashBtn.Style.Add("display", "block");
+                logOutBtn.Style.Add("display", "block");
+
+                logInBtn.Visible = false;
+                signUpBtn.Visible = false;
+
+            }
+            else
+            {
+                dashBtn.Style.Add("display", "none");
+                logOutBtn.Style.Add("display", "none");
+
+                logOutBtn.Visible = true;
+                signUpBtn.Visible = true;
+            }
+
+
             logInBtn.ServerClick += logInBtn_ServerClick;
             signUpBtn.ServerClick += signUpBtn_ServerClick;
+            logOutBtn.ServerClick += LogOutBtn_ServerClick;
+            dashBtn.ServerClick += DashBtn_ServerClick;
+
+        }
+
+        private void DashBtn_ServerClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LogOutBtn_ServerClick(object sender, EventArgs e)
+        {
+            ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "text", "alert('You will be logged out!')", true);
+            Session.Abandon();
         }
 
         private void signUpBtn_ServerClick(object sender, EventArgs e)
