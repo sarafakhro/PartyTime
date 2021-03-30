18b49cb4071e0e59191d0c4c08d30cc9c8857ad1
Sara <sara.fakhro@outlook.com>
2021-03-30 11:36:09 +0200

    Jag har fixat logik så att knapparna syns och göms beroende om en användare är inloggad eller inte.

--- PartyTime/aboutUs.aspx.cs
+++ PartyTime/aboutUs.aspx.cs
@@ -11,6 +11,32 @@ namespace PartyTime
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //om det finns en en inloggad användare, logged_User får sitt
+            //värde senare, sparas på servern
+            if(Session["logged_User"] != null)
+            {
+                //då visar vi dessa knappar
+                //vi har gömt dessa i början
+                dashBtn.Style.Add("display", "block");
+                logOutBtn.Style.Add("display", "block");
+
+                //och göm de andra knapparna
+                logInBtn.Visible = false;
+                signUpBtn.Visible = false;
+
+            }
+            //annars tvärtom
+            else
+            {
+                dashBtn.Style.Add("display", "block");
+                logOutBtn.Style.Add("display", "none");
+
+                logInBtn.Visible = true;
+                signUpBtn.Visible = true;
+            }
+
+
+
             logInBtn.ServerClick += logInBtn_ServerClick;
             signUpBtn.ServerClick += signUpBtn_ServerClick;
             logOutBtn.ServerClick += logOutBtn_ServerClick;
@@ -24,7 +50,9 @@ namespace PartyTime
 
         private void logOutBtn_ServerClick(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "text", "alert('You will be logged out')", true);
+            //reset session, när man ska logga ut, ska det inte finnas värde kvar
+            Session.Abandon();
         }
 
         private void signUpBtn_ServerClick(object sender, EventArgs e)
