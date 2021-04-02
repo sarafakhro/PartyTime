18e2a6ddcb577a371f467613e85372412f9cc84b
Sara <sara.fakhro@outlook.com>
2021-04-02 14:21:42 +0200

    Har skrivit kod för glömt lösenord.

--- PartyTime/forgotPassword.aspx.designer.cs
+++ PartyTime/forgotPassword.aspx.designer.cs
@@ -24,12 +24,39 @@ namespace PartyTime
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// password_lbl control.
+        /// email control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label password_lbl;
+        protected global::System.Web.UI.HtmlControls.HtmlInputText email;
+
+        /// <summary>
+        /// passwordLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label passwordLabel;
+
+        /// <summary>
+        /// getBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton getBtn;
+
+        /// <summary>
+        /// logInBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton logInBtn;
     }
 }
