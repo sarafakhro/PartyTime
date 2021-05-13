eff1f9333d6e3644f3c131cf62fd747271df7d18
Sara <sara.fakhro@outlook.com>
2021-05-13 18:00:35 +0200

    Rättat till klasser som inte har kodgranskats.

--- PartyTime/AboutUs.aspx.cs
+++ PartyTime/AboutUs.aspx.cs
@@ -11,32 +11,26 @@ namespace PartyTime
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //om det finns en en inloggad användare, logged_User får sitt
-            //värde senare, sparas på servern
+            //if there is an logged in user, logged_User get it's
+            //value later, save in server
             if(Session["logged_User"] != null)
             {
-                //då visar vi dessa knappar
-                //vi har gömt dessa i början
+                //show these buttons
+                //hide from the beginning
                 dashBtn.Style.Add("display", "block");
                 logOutBtn.Style.Add("display", "block");
-
-                //och göm de andra knapparna
+                //and hide these buttons
                 logInBtn.Visible = false;
                 signUpBtn.Visible = false;
-
             }
-            //annars tvärtom
+            //else vice versa
             else
             {
                 dashBtn.Style.Add("display", "none");
                 logOutBtn.Style.Add("display", "none");
-
                 logInBtn.Visible = true;
                 signUpBtn.Visible = true;
             }
-
-
-
             logInBtn.ServerClick += logInBtn_ServerClick;
             signUpBtn.ServerClick += signUpBtn_ServerClick;
             logOutBtn.ServerClick += logOutBtn_ServerClick;
@@ -51,7 +45,7 @@ namespace PartyTime
         private void logOutBtn_ServerClick(object sender, EventArgs e)
         {
             ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "text", "alert('You will be logged out')", true);
-            //reset session, när man ska logga ut, ska det inte finnas värde kvar
+            //reset session, when you want to log out, clean everything beacause we don't want any data left
             Session.Abandon();
         }
 
@@ -64,7 +58,5 @@ namespace PartyTime
         {
             Response.Redirect("logIn.aspx");
         }
-
-
     }
 }
